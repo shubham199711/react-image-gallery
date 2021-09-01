1b79b0623e5a3393435ba1e2335458d2f8357ebe
ShubhamDohare <sdohare11@gmail.com>
2021-09-01 12:26:34 +0530

    changed let to const

--- src/services/imageService.js
+++ src/services/imageService.js
@@ -5,7 +5,7 @@ const IMAGE_PER_PAGE = 50;
 const getRecentImages = async (currentPage) => {
 	try {
 		const URL = `https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&format=json&nojsoncallback=1&api_key=${process.env.REACT_APP_FLICKR_API_KEY}&page=${currentPage}&per_page=${IMAGE_PER_PAGE}`;
-		let { data: response } = await axios.get(URL, { mode: 'no-cors' });
+		const { data: response } = await axios.get(URL, { mode: 'no-cors' });
 		return response?.photos?.photo || [];
 	} catch (error) {
 		console.log(error);
@@ -15,7 +15,7 @@ const getRecentImages = async (currentPage) => {
 const getSearchedImages = async (currentPage, searchText) => {
 	try {
 		const URL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=${process.env.REACT_APP_FLICKR_API_KEY}&page=${currentPage}&text=${searchText}&per_page=${IMAGE_PER_PAGE}`;
-		let { data: response } = await axios.get(URL, { mode: 'no-cors' });
+		const { data: response } = await axios.get(URL, { mode: 'no-cors' });
 		return response?.photos?.photo || [];
 	} catch (error) {
 		console.log(error);
